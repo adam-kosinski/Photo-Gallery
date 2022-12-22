e5a03f61671b667c3247373e4bf366a112481b45
Adam Kosinski <amk119@duke.edu>
2022-12-22 04:18:18 -0500

    Better image filling using load event listener

--- main.js
+++ main.js
@@ -1,27 +1,29 @@
 window.addEventListener("load", function () {
+    addImage(0);
+});
+
+function addImage(i){
     let cols = document.querySelectorAll(".column");
     let n_cols = cols.length;
 
-    let i = 0;
+    let img = document.createElement("img");
+    img.src = "images/small/" + filenames[i];
 
-    let interval = setInterval(function(){
-
-        let img = document.createElement("img");
-        img.src = "images/small/" + filenames[i];
-
-        let shortest;
-        let min_height = Infinity;
-        for(let j = 0; j < n_cols; j++){
-            let col_height = cols[j].getBoundingClientRect().height;
-            if(col_height < min_height){
-                min_height = col_height;
-                shortest = cols[j];
-            }
+    let shortest;
+    let min_height = Infinity;
+    for(let j = 0; j < n_cols; j++){
+        let col_height = cols[j].getBoundingClientRect().height;
+        if(col_height < min_height){
+            min_height = col_height;
+            shortest = cols[j];
         }
+    }
 
-        shortest.appendChild(img);
+    shortest.appendChild(img);
 
-        i++;
-        if(i == filenames.length) clearInterval(interval);
-    }, 10);
-});
+    if(i+1 < filenames.length){
+        img.addEventListener("load", function(){
+            addImage(i+1);
+        });
+    }
+}
