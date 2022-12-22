20543337a50daebd619811a0cd8f5718cacd6093
Adam Kosinski <amk119@duke.edu>
2022-12-22 04:04:21 -0500

    Basic page layout, very nice

--- main.js
+++ main.js
@@ -1,3 +1,27 @@
-for(let filename in filenames){
-    console.log(filename);
-}
+window.addEventListener("load", function () {
+    let cols = document.querySelectorAll(".column");
+    let n_cols = cols.length;
+
+    let i = 0;
+
+    let interval = setInterval(function(){
+
+        let img = document.createElement("img");
+        img.src = "images/small/" + filenames[i];
+
+        let shortest;
+        let min_height = Infinity;
+        for(let j = 0; j < n_cols; j++){
+            let col_height = cols[j].getBoundingClientRect().height;
+            if(col_height < min_height){
+                min_height = col_height;
+                shortest = cols[j];
+            }
+        }
+
+        shortest.appendChild(img);
+
+        i++;
+        if(i == filenames.length) clearInterval(interval);
+    }, 10);
+});
